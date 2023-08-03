0d165f5ba2b9a4d97bd60c7ec213d24b23f2268e
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-03 08:59:30 +0700

    Modify Theme

--- app/src/main/java/com/muhammhassan/epatrol/ui/theme/Type.kt
+++ app/src/main/java/com/muhammhassan/epatrol/ui/theme/Type.kt
@@ -2,14 +2,24 @@ package com.muhammhassan.epatrol.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.muhammhassan.epatrol.R
 
+val PlusJakarta = FontFamily(
+    Font(R.font.poppins, FontWeight.Normal),
+    Font(R.font.poppins_black, FontWeight.Black),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_thin, FontWeight.Thin)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = PlusJakarta,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
