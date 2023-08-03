d89fc6603e17a2752855e4e2eb3c1354b82c5f4f
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-03 08:49:00 +0700

    Add Navigation Model

---
+++ app/src/main/java/com/muhammhassan/epatrol/utils/NavigationItem.kt
@@ -0,0 +1,7 @@
+package com.muhammhassan.epatrol.utils
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavigationItem(
+    val title: String, val icon: ImageVector, val screen: Screen
+)
