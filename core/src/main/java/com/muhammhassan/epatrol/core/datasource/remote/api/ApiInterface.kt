8fe0e7a09cd0884a93c9ba263d2340fe0b8a361f
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-07 12:40:47 +0700

    Synchronize patrol tas api and add verification method

--- core/src/main/java/com/muhammhassan/epatrol/core/datasource/remote/api/ApiInterface.kt
+++ core/src/main/java/com/muhammhassan/epatrol/core/datasource/remote/api/ApiInterface.kt
@@ -2,10 +2,14 @@ package com.muhammhassan.epatrol.core.datasource.remote.api
 
 import com.muhammhassan.epatrol.core.model.BaseResponse
 import com.muhammhassan.epatrol.core.model.LoginResponse
+import com.muhammhassan.epatrol.core.model.PatrolResponse
 import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface ApiInterface {
     @FormUrlEncoded
@@ -15,5 +19,9 @@ interface ApiInterface {
         @Field("password") password: String
     ): Response<BaseResponse<LoginResponse>>
 
-//    suspend fun getPatrolTask(): Response<BaseResponse<>>
+    @GET("patrol/")
+    suspend fun getPatrolTask(): Response<BaseResponse<List<PatrolResponse>>>
+
+    @PUT("patrol/{id}/verify")
+    suspend fun verifyPatrol(@Path("id") id: Long): Response<BaseResponse<Nothing>>
 }
