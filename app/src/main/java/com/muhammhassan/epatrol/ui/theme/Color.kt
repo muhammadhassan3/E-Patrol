502cfd88a1cab45d1217cbc63a893e968584f6fb
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-07 13:12:08 +0700

    Cleaning code

--- app/src/main/java/com/muhammhassan/epatrol/ui/theme/Color.kt
+++ app/src/main/java/com/muhammhassan/epatrol/ui/theme/Color.kt
@@ -2,15 +2,6 @@ package com.muhammhassan.epatrol.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-
 val Primary = Color(0xFFC1801E)
 val Secondary = Color(0xFF66420C)
 val Tertiary = Color(0xFFE47100)
@@ -21,3 +12,6 @@ val Green20 = Color(0x2414AE5C)
 
 val Red = Color(0xFFAE1414)
 val Red20 = Color(0x24AE1414)
+
+val Orange = Color(0xFFAE6914)
+val Orange20 = Color(0x24AE5C14)
