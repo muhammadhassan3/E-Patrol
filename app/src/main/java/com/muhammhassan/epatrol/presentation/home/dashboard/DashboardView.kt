39d57ea8adf07cfeb6fb64bb595d61dc58bd0216
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-07 13:12:47 +0700

    Add verification sheet

--- app/src/main/java/com/muhammhassan/epatrol/presentation/home/dashboard/DashboardView.kt
+++ app/src/main/java/com/muhammhassan/epatrol/presentation/home/dashboard/DashboardView.kt
@@ -1,8 +1,8 @@
 package com.muhammhassan.epatrol.presentation.home.dashboard
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,20 +12,27 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -37,7 +44,9 @@ import androidx.constraintlayout.compose.Dimension
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.muhammhassan.epatrol.component.LoadingDialog
 import com.muhammhassan.epatrol.component.PatrolItem
+import com.muhammhassan.epatrol.component.VerifyBottomSheetView
 import com.muhammhassan.epatrol.domain.model.PatrolModel
 import com.muhammhassan.epatrol.domain.model.UiState
 import com.muhammhassan.epatrol.domain.model.UserModel
@@ -45,27 +54,61 @@ import com.muhammhassan.epatrol.ui.theme.EPatrolTheme
 import compose.icons.Octicons
 import compose.icons.octicons.Bell24
 import compose.icons.octicons.Person24
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalGlideComposeApi::class, ExperimentalMaterialApi::class)
+@OptIn(
+    ExperimentalGlideComposeApi::class,
+    ExperimentalMaterialApi::class,
+    ExperimentalMaterial3Api::class
+)
 @Composable
 fun DashboardView(
     uiState: UiState<List<PatrolModel>>,
+    verifyState: UiState<Nothing>?,
     user: UserModel,
     onProfileClicked: () -> Unit,
     onNotificationClicked: () -> Unit,
-    onItemClicked: (patrolId: Long) -> Unit,
+    navigateToDetailPage: (patrolId: Long) -> Unit,
+    verifyUser: (id: Long) -> Unit,
     onRefreshTriggered: () -> Unit,
-    setDialogData: (value: String) -> Unit,
-    setIsDialogShow: (value: Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
+
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true,
+        confirmValueChange = { it != SheetValue.Expanded })
+    val scope = rememberCoroutineScope()
+    val isSheetShow = remember {
+        mutableStateOf(false)
+    }
+
     val username = remember { mutableStateOf("Budi Santoso") }
     val listData = remember { mutableStateListOf<PatrolModel>() }
-    val isLoading = remember { mutableStateOf(false) }
+    val swipeLoading = remember { mutableStateOf(false) }
+    val isLoading = remember {
+        mutableStateOf(false)
+    }
+
+    val isDialogShow = remember { mutableStateOf(false) }
+    val dialogMessage = remember {
+        mutableStateOf("")
+    }
 
     val scrollState = rememberLazyListState()
     val pullRefreshState =
-        rememberPullRefreshState(refreshing = isLoading.value, onRefresh = onRefreshTriggered)
+        rememberPullRefreshState(refreshing = swipeLoading.value, onRefresh = onRefreshTriggered)
+    val (selectedId, setSelectedId) = remember {
+        mutableStateOf(0L)
+    }
+    val (selectedPlate, setSelectedPlate) = remember {
+        mutableStateOf("")
+    }
+
+    val context = LocalContext.current
+
+    LaunchedEffect(key1 = true, block = {
+        sheetState.hide()
+    })
 
     LaunchedEffect(key1 = user, block = {
         val tmpName = StringBuilder(user.name).append("!")
@@ -75,11 +118,34 @@ fun DashboardView(
     LaunchedEffect(key1 = uiState, block = {
         when (uiState) {
             is UiState.Error -> {
+                swipeLoading.value = false
+                dialogMessage.value = uiState.message
+                isDialogShow.value = true
+            }
+
+            UiState.Loading -> {
+                swipeLoading.value = true
+            }
+
+            is UiState.Success -> {
+                swipeLoading.value = false
+                listData.clear()
+                uiState.data?.let {
+                    listData.addAll(
+                        it
+                    )
+                }
+
+            }
+        }
+    })
+
+    LaunchedEffect(key1 = verifyState, block = {
+        when (verifyState) {
+            is UiState.Error -> {
                 isLoading.value = false
-                setDialogData.invoke(
-                    uiState.message
-                )
-                setIsDialogShow.invoke(true)
+                dialogMessage.value = verifyState.message
+                isDialogShow.value = true
             }
 
             UiState.Loading -> {
@@ -88,18 +154,41 @@ fun DashboardView(
 
             is UiState.Success -> {
                 isLoading.value = false
-                listData.clear()
-                listData.addAll(
-                    uiState.data
-                )
-
+                navigateToDetailPage.invoke(selectedId)
+                Toast.makeText(context, "Tugas terverifikasi", Toast.LENGTH_SHORT).show()
             }
+
+            null -> {}
         }
     })
 
+    if (isSheetShow.value) {
+        ModalBottomSheet(
+            onDismissRequest = { isSheetShow.value = false },
+            sheetState = sheetState,
+        ) {
+            VerifyBottomSheetView(plate = selectedPlate, onSwipe = {
+                scope.launch { sheetState.hide() }
+                isSheetShow.value = false
+                verifyUser.invoke(selectedId)
+            })
+        }
+    }
+
+    if (isDialogShow.value) {
+        AlertDialog(onDismissRequest = { isDialogShow.value = false }, confirmButton = {
+            TextButton(onClick = { isDialogShow.value = false }) {
+                Text(text = "Oke")
+            }
+        }, title = { Text(text = "Pemberitahuan") }, text = { Text(text = dialogMessage.value) })
+    }
+
+    if (isLoading.value) {
+        LoadingDialog(onDismiss = { isLoading.value = false })
+    }
 
     ConstraintLayout(modifier = modifier.fillMaxWidth()) {
-        val (greetings, name, notification, profile, title, details, content) = createRefs()
+        val (greetings, name, notification, profile, title, details, content, indicator) = createRefs()
         Text(text = "Halo,", modifier = Modifier.constrainAs(greetings) {
             start.linkTo(parent.start, 16.dp)
             top.linkTo(parent.top, 16.dp)
@@ -174,27 +263,48 @@ fun DashboardView(
             )
         )
 
-        Box(modifier = Modifier
-            .constrainAs(content) {
-                top.linkTo(title.bottom, 8.dp)
-                start.linkTo(parent.start, 16.dp)
-                end.linkTo(parent.end, 16.dp)
-                bottom.linkTo(parent.bottom)
-                width = Dimension.fillToConstraints
-                height = Dimension.fillToConstraints
-            }
-            .pullRefresh(pullRefreshState)) {
-            LazyColumn(content = {
+        LazyColumn(
+            content = {
                 items(listData, key = { it.id }) {
-                    PatrolItem(model = it, onItemClick = { /*TODO*/ }, modifier = Modifier)
+                    PatrolItem(model = it, onItemClick = { id, plate ->
+                        if (it.verified) {
+                            navigateToDetailPage.invoke(id)
+                            return@PatrolItem
+                        }
+                        if (it.lead == user.email) {
+                            scope.launch { sheetState.show() }
+                            isSheetShow.value = true
+                            setSelectedId(id)
+                            setSelectedPlate(plate)
+                        } else {
+                            dialogMessage.value =
+                                "Perlengkapan patroli ini belum dicek, silahkan tunggu ketua regu untuk melakukan verifikasi kemudian lakukan refresh pada halaman ini."
+                            isDialogShow.value = true
+                        }
+                    }, modifier = Modifier)
                 }
-            }, state = scrollState, verticalArrangement = Arrangement.spacedBy(8.dp), modifier = Modifier.fillMaxSize())
-            PullRefreshIndicator(
-                refreshing = isLoading.value, state = pullRefreshState, modifier = Modifier.align(
-                    Alignment.TopCenter
-                )
-            )
-        }
+            },
+            state = scrollState,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .constrainAs(content) {
+                    top.linkTo(title.bottom, 8.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                    width = Dimension.fillToConstraints
+                    height = Dimension.fillToConstraints
+                }
+                .pullRefresh(pullRefreshState),
+            contentPadding = PaddingValues(start = 16.dp, end = 16.dp, top = 0.dp, bottom = 16.dp)
+        )
+        PullRefreshIndicator(
+            refreshing = swipeLoading.value, state = pullRefreshState, modifier = Modifier.constrainAs(indicator){
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                top.linkTo(content.top)
+            }
+        )
     }
 }
 
@@ -212,15 +322,17 @@ fun DashboardPreview() {
                     "07:00",
                     "ade@email.co",
                     false,
-                    "Jl juanda"
+                    "Jl juanda",
+                    "R 0000 PH"
                 )
             )
         ),
             user = UserModel("Doni Salamander", "", ""),
             onProfileClicked = {},
-            onItemClicked = {},
+            navigateToDetailPage = {},
             onNotificationClicked = {},
-            onRefreshTriggered = {}, setDialogData = {}, setIsDialogShow = {}
+            onRefreshTriggered = {},
+            verifyUser = {}, verifyState = null
         )
     }
 }
