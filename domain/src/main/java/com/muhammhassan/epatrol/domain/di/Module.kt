4722c72a6b1de9d78fba04eda16ab0a2ad5a5a3e
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-07 18:01:32 +0700

    Add Module

--- domain/src/main/java/com/muhammhassan/epatrol/domain/di/Module.kt
+++ domain/src/main/java/com/muhammhassan/epatrol/domain/di/Module.kt
@@ -6,9 +6,11 @@ import com.muhammhassan.epatrol.core.di.Module.provideApi
 import com.muhammhassan.epatrol.core.di.Module.repositoryModule
 import com.muhammhassan.epatrol.domain.interactor.DashboardInteractor
 import com.muhammhassan.epatrol.domain.interactor.LoginInteractor
+import com.muhammhassan.epatrol.domain.interactor.PatrolDetailInteractor
 import com.muhammhassan.epatrol.domain.interactor.TaskListInteractor
 import com.muhammhassan.epatrol.domain.usecase.DashboardUseCase
 import com.muhammhassan.epatrol.domain.usecase.LoginUseCase
+import com.muhammhassan.epatrol.domain.usecase.PatrolDetailUseCase
 import com.muhammhassan.epatrol.domain.usecase.TaskListUseCase
 import org.koin.dsl.module
 
@@ -17,6 +19,7 @@ object Module {
         single<LoginUseCase> { LoginInteractor(get()) }
         single<DashboardUseCase> { DashboardInteractor(get(), get()) }
         single<TaskListUseCase> { TaskListInteractor(get(), get()) }
+        single<PatrolDetailUseCase> { PatrolDetailInteractor(get()) }
     }
 
     fun retrofit(version: Int) = provideApi(version)
