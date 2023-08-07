39f5ec6b7109dbcf20bb203648d10b96bbee8b73
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-07 13:16:39 +0700

    Change view model scope

--- app/src/main/java/com/muhammhassan/epatrol/di/Module.kt
+++ app/src/main/java/com/muhammhassan/epatrol/di/Module.kt
@@ -3,12 +3,13 @@ package com.muhammhassan.epatrol.di
 import com.muhammhassan.epatrol.presentation.auth.login.LoginViewModel
 import com.muhammhassan.epatrol.presentation.home.dashboard.DashboardViewModel
 import com.muhammhassan.epatrol.presentation.home.task.TaskListViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 object Module {
     val viewModelModule = module {
-        single { LoginViewModel(get()) }
-        single { DashboardViewModel(get()) }
-        single { TaskListViewModel(get()) }
+        viewModel { LoginViewModel(get()) }
+        viewModel { DashboardViewModel(get()) }
+        viewModel { TaskListViewModel(get()) }
     }
 }
