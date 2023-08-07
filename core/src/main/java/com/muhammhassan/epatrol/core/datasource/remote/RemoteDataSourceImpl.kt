c4ec500597a52fd5decd447acadcaa7e27396bc7
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-07 13:24:01 +0700

    Add verification method

--- core/src/main/java/com/muhammhassan/epatrol/core/datasource/remote/RemoteDataSourceImpl.kt
+++ core/src/main/java/com/muhammhassan/epatrol/core/datasource/remote/RemoteDataSourceImpl.kt
@@ -5,11 +5,9 @@ import com.muhammhassan.epatrol.core.model.ApiResponse
 import com.muhammhassan.epatrol.core.model.LoginResponse
 import com.muhammhassan.epatrol.core.model.PatrolResponse
 import com.muhammhassan.epatrol.core.utils.Utils.parseError
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
-import timber.log.Timber
 
 class RemoteDataSourceImpl(private val api: ApiInterface) : RemoteDataSource {
     override suspend fun login(email: String, password: String): Flow<ApiResponse<LoginResponse>> =
@@ -32,48 +30,35 @@ class RemoteDataSourceImpl(private val api: ApiInterface) : RemoteDataSource {
         }
 
     override suspend fun getTaskList(): Flow<ApiResponse<List<PatrolResponse>>> =
-        flow<ApiResponse<List<PatrolResponse>>> {
-            Timber.e("Invoked")
+        flow {
             emit(ApiResponse.Loading)
-            delay(800)
-            emit(
-                ApiResponse.Success(
-                    listOf(
-                        PatrolResponse(
-                            status = "belum-dikerjakan",
-                            alamat = "Jl Juanda",
-                            jam = "20:00",
-                            judul = "Patroli Rutin",
-                            ketua = "ade@email.co",
-                            tanggal = "20 Juli 2023",
-                            verified = false,
-                            id = 1
-                        ), PatrolResponse(
-                            status = "belum-dikerjakan",
-                            alamat = "Jl Gatot Subroto",
-                            jam = "15:00",
-                            judul = "Patroli Rutin Harian",
-                            ketua = "budi@email.co",
-                            tanggal = "21 Juli 2023",
-                            verified = false,
-                            id = 2
-                        ), PatrolResponse(
-                            status = "sudah-dikerjakan",
-                            alamat = "Jl Ahmad yani",
-                            jam = "14:00",
-                            judul = "Patroli Rutin Mingguan",
-                            ketua = "budi@email.co",
-                            tanggal = "19 Juli 2023",
-                            verified = true,
-                            id = 3
-                        )
-                    )
-                )
-            )
+            val response = api.getPatrolTask()
+            if(response.isSuccessful){
+                response.body()?.data?.let {
+                    emit(ApiResponse.Success(it))
+                }
+            }else{
+                emit(ApiResponse.Error(response.parseError()))
+            }
         }.catch { error ->
             error.printStackTrace()
             error.message?.let {
                 emit(ApiResponse.Error(it))
             }
         }
+
+    override suspend fun verifyPatrol(id: Long): Flow<ApiResponse<Nothing>> = flow{
+        emit(ApiResponse.Loading)
+        val response = api.verifyPatrol(id)
+        if(response.isSuccessful){
+            emit(ApiResponse.Success(null))
+        }else{
+            emit(ApiResponse.Error(response.parseError()))
+        }
+    }.catch {
+        it.printStackTrace()
+        it.message?.let{message ->
+            emit(ApiResponse.Error(message))
+        }
+    }
 }
