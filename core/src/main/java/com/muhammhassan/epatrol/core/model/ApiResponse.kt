fb469ef6b6e097bfb00ab7156607649d2b98feeb
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-07 12:41:01 +0700

    Change to nullable to afford null response

--- core/src/main/java/com/muhammhassan/epatrol/core/model/ApiResponse.kt
+++ core/src/main/java/com/muhammhassan/epatrol/core/model/ApiResponse.kt
@@ -2,6 +2,6 @@ package com.muhammhassan.epatrol.core.model
 
 sealed class ApiResponse<out T> {
     object Loading: ApiResponse<Nothing>()
-    data class Success<out T>(val data: T): ApiResponse<T>()
+    data class Success<out T>(val data: T?): ApiResponse<T>()
     data class Error(val message: String): ApiResponse<Nothing>()
 }
