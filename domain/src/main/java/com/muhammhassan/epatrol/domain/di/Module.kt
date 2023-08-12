2f747420d161b948e90c32e8cbb510038077fd3b
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-12 22:34:42 +0700

    Add Delete Use Case

--- domain/src/main/java/com/muhammhassan/epatrol/domain/di/Module.kt
+++ domain/src/main/java/com/muhammhassan/epatrol/domain/di/Module.kt
@@ -5,10 +5,12 @@ import com.muhammhassan.epatrol.core.di.Module.datastoreModule
 import com.muhammhassan.epatrol.core.di.Module.provideApi
 import com.muhammhassan.epatrol.core.di.Module.repositoryModule
 import com.muhammhassan.epatrol.domain.interactor.DashboardInteractor
+import com.muhammhassan.epatrol.domain.interactor.DetailPatrolEventInteractor
 import com.muhammhassan.epatrol.domain.interactor.LoginInteractor
 import com.muhammhassan.epatrol.domain.interactor.PatrolDetailInteractor
 import com.muhammhassan.epatrol.domain.interactor.TaskListInteractor
 import com.muhammhassan.epatrol.domain.usecase.DashboardUseCase
+import com.muhammhassan.epatrol.domain.usecase.DetailPatrolEventUseCase
 import com.muhammhassan.epatrol.domain.usecase.LoginUseCase
 import com.muhammhassan.epatrol.domain.usecase.PatrolDetailUseCase
 import com.muhammhassan.epatrol.domain.usecase.TaskListUseCase
@@ -20,6 +22,7 @@ object Module {
         single<DashboardUseCase> { DashboardInteractor(get(), get()) }
         single<TaskListUseCase> { TaskListInteractor(get(), get()) }
         single<PatrolDetailUseCase> { PatrolDetailInteractor(get(), get()) }
+        single<DetailPatrolEventUseCase> { DetailPatrolEventInteractor(get(), get()) }
     }
 
     fun retrofit(version: Int) = provideApi(version)
