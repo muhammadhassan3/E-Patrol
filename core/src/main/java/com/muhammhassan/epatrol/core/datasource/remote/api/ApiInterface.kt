250c68757b2d8b37b546395c558fc79681259edf
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-14 22:49:21 +0700

    Create add patrol event function

--- core/src/main/java/com/muhammhassan/epatrol/core/datasource/remote/api/ApiInterface.kt
+++ core/src/main/java/com/muhammhassan/epatrol/core/datasource/remote/api/ApiInterface.kt
@@ -4,13 +4,17 @@ import com.muhammhassan.epatrol.core.model.BaseResponse
 import com.muhammhassan.epatrol.core.model.LoginResponse
 import com.muhammhassan.epatrol.core.model.PatrolDetailResponse
 import com.muhammhassan.epatrol.core.model.PatrolResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.DELETE
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Part
 import retrofit2.http.Path
 
 interface ApiInterface {
@@ -32,4 +36,8 @@ interface ApiInterface {
 
     @DELETE("patrol/{patrolId}/{eventId}")
     suspend fun deleteEvent(@Path("patrolId") patrolId: Long, @Path("eventId") eventId: Long): Response<BaseResponse<Nothing>>
+
+    @Multipart
+    @POST("patrol/{patrolId}/")
+    suspend fun addEvent(@Path("patrolId") patrolId: Long, @Part image: MultipartBody.Part, @Part("event") event: RequestBody, @Part("action") action: RequestBody, @Part("summary") summary: RequestBody): Response<BaseResponse<Nothing>>
 }
