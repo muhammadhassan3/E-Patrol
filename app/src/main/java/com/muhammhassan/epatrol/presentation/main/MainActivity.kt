873d8745555c6f449d49c3fae1c3679d3a34d126
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-15 23:22:26 +0700

    Add Notification permission for android 13

--- app/src/main/java/com/muhammhassan/epatrol/presentation/main/MainActivity.kt
+++ app/src/main/java/com/muhammhassan/epatrol/presentation/main/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.muhammhassan.epatrol.presentation.main
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.muhammhassan.epatrol.presentation.auth.AuthActivity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
+    private val permissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                navigate()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Silahkan aktifkan perizinan notifikasi pada halaman pengaturan",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         splashScreen.setKeepOnScreenCondition {
@@ -16,6 +35,23 @@ class MainActivity : ComponentActivity() {
         }
         super.onCreate(savedInstanceState)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                navigate()
+            } else {
+                permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }else{
+            navigate()
+        }
+    }
+
+
+    private fun navigate() {
         runBlocking {
             delay(1200)
             startActivity(Intent(this@MainActivity, AuthActivity::class.java))
