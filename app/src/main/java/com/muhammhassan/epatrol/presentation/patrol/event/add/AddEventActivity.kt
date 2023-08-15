bf8b7abd3906e9ef122a3cccae32b573ed403829
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-15 17:47:15 +0700

    Add Location feature for added event

--- app/src/main/java/com/muhammhassan/epatrol/presentation/patrol/event/add/AddEventActivity.kt
+++ app/src/main/java/com/muhammhassan/epatrol/presentation/patrol/event/add/AddEventActivity.kt
@@ -1,24 +1,53 @@
 package com.muhammhassan.epatrol.presentation.patrol.event.add
 
+import android.Manifest
 import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.getValue
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.core.net.toUri
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.Priority
 import com.muhammhassan.epatrol.utils.createCustomTempFile
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 class AddEventActivity : ComponentActivity() {
 
     private val viewModel by viewModel<AddEventViewModel>()
     private lateinit var currentPath: String
+    private var fusedLocation: FusedLocationProviderClient? = null
+    private lateinit var locationRequest: LocationRequest
+    private val locationCallback: LocationCallback by lazy {
+        object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+                for (location in p0.locations) {
+                    Timber.i("Location Updated : ${location.latitude}/${location.longitude}")
+                    viewModel.latitude = location.latitude
+                    viewModel.longitude = location.longitude
+                }
+            }
+        }
+    }
 
     private val cameraLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
@@ -26,6 +55,37 @@ class AddEventActivity : ComponentActivity() {
                 Timber.i("Image loaded")
                 val file = File(currentPath)
                 viewModel.setImage(file.toUri())
+            }
+        }
+
+    private val locationIntentCallback =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+            when (result.resultCode) {
+                RESULT_OK -> {
+                    Timber.i("Location Activated")
+                }
+
+                RESULT_CANCELED -> {
+                    Timber.e("Location request canceled")
+                }
+            }
+        }
+
+    private val permissionCallback =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            val result = it.all { permission ->
+                permission.value
+            }
+
+            if (result) {
+                startLocationUpdate()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Perizinan ditolak, silahkan aktifkan perizinan lokasi pada pengaturan",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
             }
         }
 
@@ -52,6 +112,9 @@ class AddEventActivity : ComponentActivity() {
                     viewModel.save(this@AddEventActivity)
                 })
         }
+
+        initLocation()
+        startLocationUpdate()
     }
 
     private fun openIntentCamera() {
@@ -71,7 +134,65 @@ class AddEventActivity : ComponentActivity() {
         }
     }
 
+    private fun initLocation() {
+        fusedLocation = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest =
+            LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, TimeUnit.SECONDS.toMillis(2))
+                .build()
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client = LocationServices.getSettingsClient(this)
+
+        client.checkLocationSettings(builder.build())
+            .addOnSuccessListener {
+
+            }.addOnFailureListener {
+                if (it is ResolvableApiException) {
+                    try {
+                        locationIntentCallback.launch(
+                            IntentSenderRequest.Builder(it.resolution).build()
+                        )
+                    } catch (e: IntentSender.SendIntentException) {
+                        Timber.e(e)
+                        Toast.makeText(this, "Gagal menyelesaikan masalah", Toast.LENGTH_SHORT)
+                            .show()
+                        finish()
+                    }
+                }
+            }
+    }
+
+    private fun startLocationUpdate() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocation?.requestLocationUpdates(locationRequest, locationCallback, mainLooper)
+        } else {
+            permissionCallback.launch(REQUIRED_LOCATION_PERMISSION)
+        }
+    }
+
+    private fun stopLocationUpdate() {
+        fusedLocation?.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopLocationUpdate()
+    }
+
+
     companion object {
         const val patrolId = "patrol_id"
+        private val REQUIRED_LOCATION_PERMISSION = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
     }
 }
