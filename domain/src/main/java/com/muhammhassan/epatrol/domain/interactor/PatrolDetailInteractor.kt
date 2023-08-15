27fb0abc0e8a1eab15f620a60c50938ecba14f05
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-15 17:51:57 +0700

    Fix clean arch and add mark as done function

--- domain/src/main/java/com/muhammhassan/epatrol/domain/interactor/PatrolDetailInteractor.kt
+++ domain/src/main/java/com/muhammhassan/epatrol/domain/interactor/PatrolDetailInteractor.kt
@@ -1,7 +1,7 @@
 package com.muhammhassan.epatrol.domain.interactor
 
-import com.muhammhassan.epatrol.core.datasource.local.LocalDataSource
-import com.muhammhassan.epatrol.core.datasource.remote.RemoteDataSource
+import com.muhammhassan.epatrol.core.repository.TaskRepository
+import com.muhammhassan.epatrol.core.repository.UserRepository
 import com.muhammhassan.epatrol.domain.model.PatrolDetailModel
 import com.muhammhassan.epatrol.domain.model.PatrolEventModel
 import com.muhammhassan.epatrol.domain.model.UiState
@@ -11,11 +11,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class PatrolDetailInteractor(
-    private val remote: RemoteDataSource,
-    private val local: LocalDataSource
+    private val task: TaskRepository,
+    private val user: UserRepository
 ) : PatrolDetailUseCase {
     override suspend fun getDetailPatrol(id: Long): Flow<UiState<PatrolDetailModel>> {
-        return remote.getPatrolDetail(id).map {
+        return task.getDetailPatrol(id).map {
             it.mapToUiState { data ->
                 PatrolDetailModel(
                     data.id,
@@ -37,13 +37,21 @@ class PatrolDetailInteractor(
                             event.createdAt,
                             author = event.author
                         )
-                    }, lead = data.lead)
+                    }, lead = data.lead
+                )
             }
         }
     }
 
     override suspend fun getSavedEmail(): Flow<String?> {
-        return local.getEmail()
+        return user.getEmail()
     }
 
+    override suspend fun markAsDone(id: Long): Flow<UiState<Nothing>> {
+        return task.markAsDone(id).map {
+            it.mapToUiState { data ->
+                data
+            }
+        }
+    }
 }
