15f0ebb1adc5de6c8fa918676f553228da6d2373
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-16 20:01:08 +0700

    Fix constant type

--- app/src/main/java/com/muhammhassan/epatrol/utils/PatrolStatus.kt
+++ app/src/main/java/com/muhammhassan/epatrol/utils/PatrolStatus.kt
@@ -1,7 +1,7 @@
 package com.muhammhassan.epatrol.utils
 
 object PatrolStatus {
-    const val BELUM_DIJALANKAN = "belum-dikerjalan"
+    const val BELUM_DIJALANKAN = "belum-dikerjakan"
     const val SUDAH_DIJALANKAN = "sudah-dikerjakan"
     const val SEDANG_DIJALANKAN = "sedang-dikerjakan"
 }
