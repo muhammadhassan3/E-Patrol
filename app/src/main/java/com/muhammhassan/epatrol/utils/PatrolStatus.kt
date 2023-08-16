615e819e42230694cca53a6d0f57338d17c856c5
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-16 13:26:31 +0700

    Create status constant

---
+++ app/src/main/java/com/muhammhassan/epatrol/utils/PatrolStatus.kt
@@ -0,0 +1,7 @@
+package com.muhammhassan.epatrol.utils
+
+object PatrolStatus {
+    const val BELUM_DIJALANKAN = "belum-dikerjalan"
+    const val SUDAH_DIJALANKAN = "sudah-dikerjakan"
+    const val SEDANG_DIJALANKAN = "sedang-dikerjakan"
+}
