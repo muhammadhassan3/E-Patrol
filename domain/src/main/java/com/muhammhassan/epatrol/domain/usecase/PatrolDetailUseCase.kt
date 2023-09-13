90feeda5f74befb76b5cd44506a5f1414768a85b
muhammhassan <muhammadhassan7788@gmail.com>
2023-09-13 12:43:14 +0700

    add interface function

--- domain/src/main/java/com/muhammhassan/epatrol/domain/usecase/PatrolDetailUseCase.kt
+++ domain/src/main/java/com/muhammhassan/epatrol/domain/usecase/PatrolDetailUseCase.kt
@@ -1,11 +1,14 @@
 package com.muhammhassan.epatrol.domain.usecase
 
 import com.muhammhassan.epatrol.domain.model.PatrolDetailModel
+import com.muhammhassan.epatrol.domain.model.PatrolEventModel
 import com.muhammhassan.epatrol.domain.model.UiState
 import kotlinx.coroutines.flow.Flow
 
 interface PatrolDetailUseCase {
     suspend fun getDetailPatrol(id: Long): Flow<UiState<PatrolDetailModel>>
+
+    suspend fun getEventList(patrolId: Long): Flow<UiState<List<PatrolEventModel>>>
 
     suspend fun getSavedEmail(): Flow<String?>
 
