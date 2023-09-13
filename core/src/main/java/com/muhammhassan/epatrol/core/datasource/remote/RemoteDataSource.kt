9258138904a9b8afee310fcd5300b289926d919a
muhammhassan <muhammadhassan7788@gmail.com>
2023-09-13 12:44:46 +0700

    Add API interface function

--- core/src/main/java/com/muhammhassan/epatrol/core/datasource/remote/RemoteDataSource.kt
+++ core/src/main/java/com/muhammhassan/epatrol/core/datasource/remote/RemoteDataSource.kt
@@ -1,16 +1,21 @@
 package com.muhammhassan.epatrol.core.datasource.remote
 
 import com.muhammhassan.epatrol.core.model.ApiResponse
+import com.muhammhassan.epatrol.core.model.EventDetailResponse
 import com.muhammhassan.epatrol.core.model.LoginResponse
 import com.muhammhassan.epatrol.core.model.PatrolDetailResponse
-import com.muhammhassan.epatrol.core.model.PatrolResponse
+import com.muhammhassan.epatrol.core.model.PatrolEventData
+import com.muhammhassan.epatrol.core.model.PatrolItemResponse
 import kotlinx.coroutines.flow.Flow
 import java.io.File
 
 interface RemoteDataSource {
     suspend fun login(email: String, password: String): Flow<ApiResponse<LoginResponse>>
 
-    suspend fun getTaskList(): Flow<ApiResponse<List<PatrolResponse>>>
+    suspend fun getTaskList(): Flow<ApiResponse<List<PatrolItemResponse>>>
+
+    suspend fun getTaskEventList(patrolId: Long): Flow<ApiResponse<List<PatrolEventData>>>
+    suspend fun getEventDetail(eventId: Long): Flow<ApiResponse<EventDetailResponse>>
 
     suspend fun verifyPatrol(id: Long): Flow<ApiResponse<Nothing>>
 
@@ -25,7 +30,8 @@ interface RemoteDataSource {
         action: String,
         image: File,
         lat: Double,
-        long: Double
+        long: Double,
+        authorName: String
     ): Flow<ApiResponse<Nothing>>
 
     suspend fun markAsDonePatrol(
