8479349fc5ffbe2899e04739d8e136b0843bf7a1
muhammhassan <muhammadhassan7788@gmail.com>
2023-09-13 12:33:25 +0700

    fix endpoint url

--- core/src/main/java/com/muhammhassan/epatrol/core/datasource/remote/api/ApiInterface.kt
+++ core/src/main/java/com/muhammhassan/epatrol/core/datasource/remote/api/ApiInterface.kt
@@ -1,8 +1,10 @@
 package com.muhammhassan.epatrol.core.datasource.remote.api
 
 import com.muhammhassan.epatrol.core.model.BaseResponse
+import com.muhammhassan.epatrol.core.model.EventDetailResponse
 import com.muhammhassan.epatrol.core.model.LoginResponse
 import com.muhammhassan.epatrol.core.model.PatrolDetailResponse
+import com.muhammhassan.epatrol.core.model.PatrolEventResponse
 import com.muhammhassan.epatrol.core.model.PatrolResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -16,6 +18,7 @@ import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Part
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiInterface {
     @FormUrlEncoded
@@ -24,33 +27,58 @@ interface ApiInterface {
         @Field("email") email: String, @Field("password") password: String
     ): Response<BaseResponse<LoginResponse>>
 
-    @GET("patrol/")
-    suspend fun getPatrolTask(): Response<BaseResponse<List<PatrolResponse>>>
+    @GET("patroli")
+    suspend fun getPatrolTask(): Response<BaseResponse<PatrolResponse>>
 
-    @PUT("patrol/{id}/verify")
-    suspend fun verifyPatrol(@Path("id") id: Long): Response<BaseResponse<Nothing>>
+    @PUT("patroli/{id}")
+    suspend fun verifyPatrol(
+        @Path("id") id: Long, @Query("status") status: String = "sedang-dikerjakan"
+    ): Response<BaseResponse<Nothing>>
 
-    @GET("patrol/{id}")
-    suspend fun getDetail(@Path("id") id: Long): Response<BaseResponse<PatrolDetailResponse>>
+    @GET("patroli/{id}")
+    suspend fun getPatrolDetail(@Path("id") id: Long): Response<BaseResponse<PatrolDetailResponse>>
 
+    //Unavaiable Endpoint
+    @GET("kejadian-patroli/list-kejadian/{id}")
+    suspend fun getPatrolEvent(@Path("id") id: Long): Response<BaseResponse<PatrolEventResponse>>
+
+    @GET("kejadian-patroli/{id}")
+    suspend fun getEventDetail(@Path("id") eventId: Long): Response<BaseResponse<EventDetailResponse>>
+
+    //Uncheck endpoint
     @DELETE("patrol/{patrolId}/{eventId}")
     suspend fun deleteEvent(
-        @Path("patrolId") patrolId: Long,
-        @Path("eventId") eventId: Long
+        @Path("patrolId") patrolId: Long, @Path("eventId") eventId: Long
     ): Response<BaseResponse<Nothing>>
 
     @Multipart
-    @POST("patrol/{patrolId}/")
+    @POST("kejadian-patroli")
     suspend fun addEvent(
-        @Path("patrolId") patrolId: Long,
+        @Part("id_patroli") patrolId: RequestBody,
         @Part image: MultipartBody.Part,
-        @Part("event") event: RequestBody,
-        @Part("action") action: RequestBody,
-        @Part("summary") summary: RequestBody,
-        @Part("lat") latitude: RequestBody,
-        @Part("long") longitude: RequestBody
+        @Part("penemuan_kejadian") event: RequestBody,
+        @Part("tindakan") action: RequestBody,
+        @Part("uraian_kejadian") summary: RequestBody,
+        @Part("latitude") latitude: RequestBody,
+        @Part("longitude") longitude: RequestBody,
+        @Part("writer") author: RequestBody
     ): Response<BaseResponse<Nothing>>
 
-    @POST("patrol/{id}/finish")
-    suspend fun markAsDonePatrol(@Path("id") id: Long): Response<BaseResponse<Nothing>>
+    @FormUrlEncoded
+    @POST("kejadian-patroli")
+    suspend fun addEventRequestBody(
+        @Field("id_patroli") patrolId: Long,
+        @Field("foto") image: String,
+        @Field("penemuan_kejadian") event: String,
+        @Field("tindakan") action: String,
+        @Field("uraian_kejadian") summary: String,
+        @Field("latitude") latitude: Double,
+        @Field("longitude") longitude: Double,
+        @Field("writer") author: String
+    ): Response<BaseResponse<Nothing>>
+
+    @PUT("patroli/{id}")
+    suspend fun markAsDonePatrol(
+        @Path("id") id: Long, @Query("status") status: String = "sudah-dikerjakan"
+    ): Response<BaseResponse<Nothing>>
 }
