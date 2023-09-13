1d9dc03cd89c47b368abfe807fbe3467b00f60aa
muhammhassan <muhammadhassan7788@gmail.com>
2023-09-13 12:46:11 +0700

    Add authorname to parameter

--- core/src/main/java/com/muhammhassan/epatrol/core/repository/TaskRepository.kt
+++ core/src/main/java/com/muhammhassan/epatrol/core/repository/TaskRepository.kt
@@ -1,17 +1,23 @@
 package com.muhammhassan.epatrol.core.repository
 
 import com.muhammhassan.epatrol.core.model.ApiResponse
+import com.muhammhassan.epatrol.core.model.EventDetailResponse
 import com.muhammhassan.epatrol.core.model.PatrolDetailResponse
-import com.muhammhassan.epatrol.core.model.PatrolResponse
+import com.muhammhassan.epatrol.core.model.PatrolEventData
+import com.muhammhassan.epatrol.core.model.PatrolItemResponse
 import kotlinx.coroutines.flow.Flow
 import java.io.File
 
 interface TaskRepository {
-    suspend fun getTaskList(): Flow<ApiResponse<List<PatrolResponse>>>
+    suspend fun getTaskList(): Flow<ApiResponse<List<PatrolItemResponse>>>
 
     suspend fun verifyPatrolTask(id: Long): Flow<ApiResponse<Nothing>>
 
     suspend fun getDetailPatrol(id: Long): Flow<ApiResponse<PatrolDetailResponse>>
+
+    suspend fun getEventList(patrolId: Long): Flow<ApiResponse<List<PatrolEventData>>>
+
+    suspend fun getEventDetail(eventId: Long): Flow<ApiResponse<EventDetailResponse>>
 
     suspend fun deletePatrolEvent(patrolId: Long, eventId: Long): Flow<ApiResponse<Nothing>>
 
@@ -22,7 +28,8 @@ interface TaskRepository {
         action: String,
         image: File,
         latitude: Double,
-        longitude: Double
+        longitude: Double,
+        authorName: String
     ): Flow<ApiResponse<Nothing>>
 
     suspend fun markAsDone(patrolId: Long): Flow<ApiResponse<Nothing>>
