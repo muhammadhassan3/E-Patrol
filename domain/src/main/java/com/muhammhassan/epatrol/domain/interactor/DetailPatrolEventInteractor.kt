cb1f6b7e5f164fd0275000a03bcc760e12f8fb83
muhammhassan <muhammadhassan7788@gmail.com>
2023-09-13 12:34:47 +0700

    Add detail content from api

--- domain/src/main/java/com/muhammhassan/epatrol/domain/interactor/DetailPatrolEventInteractor.kt
+++ domain/src/main/java/com/muhammhassan/epatrol/domain/interactor/DetailPatrolEventInteractor.kt
@@ -2,6 +2,7 @@ package com.muhammhassan.epatrol.domain.interactor
 
 import com.muhammhassan.epatrol.core.repository.TaskRepository
 import com.muhammhassan.epatrol.core.repository.UserRepository
+import com.muhammhassan.epatrol.domain.model.EventDetailModel
 import com.muhammhassan.epatrol.domain.model.UiState
 import com.muhammhassan.epatrol.domain.usecase.DetailPatrolEventUseCase
 import com.muhammhassan.epatrol.domain.utils.Mapper.mapToUiState
@@ -23,6 +24,24 @@ class DetailPatrolEventInteractor(
     override suspend fun getEmail(): Flow<String> {
         return user.getEmail().map {
             it ?: ""
+        }
+    }
+
+    override suspend fun getEventDetail(eventId: Long): Flow<UiState<EventDetailModel>> {
+        return task.getEventDetail(eventId).map {
+            it.mapToUiState {data ->
+                EventDetailModel(
+                    id = data.id,
+                    title = data.title,
+                    createdAt = data.createdAt,
+                    author = data.author,
+                    action = data.action,
+                    summary = data.summary,
+                    long = data.long,
+                    lat = data.lat,
+                    image = data.image
+                )
+            }
         }
     }
 
