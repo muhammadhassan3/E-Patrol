f1439dca6aa5fa8f38f5a640ed19460332b95c50
muhammhassan <muhammadhassan7788@gmail.com>
2023-09-13 12:44:05 +0700

    Cleaning code

--- domain/src/main/java/com/muhammhassan/epatrol/domain/model/PatrolEventModel.kt
+++ domain/src/main/java/com/muhammhassan/epatrol/domain/model/PatrolEventModel.kt
@@ -1,17 +1,10 @@
 package com.muhammhassan.epatrol.domain.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class PatrolEventModel(
     val id: Long,
-    val image: String,
     val summary: String,
     val title: String,
     val action: String,
-    val lat: Double,
-    val long: Double,
     val createdAt: String,
     val author: String,
-): Parcelable
+)
