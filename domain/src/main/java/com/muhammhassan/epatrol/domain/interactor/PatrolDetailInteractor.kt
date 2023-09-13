b4086759ff7dae209f76e17db744dac8b6671748
muhammhassan <muhammadhassan7788@gmail.com>
2023-09-13 12:42:13 +0700

    Modify response for ui state

--- domain/src/main/java/com/muhammhassan/epatrol/domain/interactor/PatrolDetailInteractor.kt
+++ domain/src/main/java/com/muhammhassan/epatrol/domain/interactor/PatrolDetailInteractor.kt
@@ -11,34 +11,38 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class PatrolDetailInteractor(
-    private val task: TaskRepository,
-    private val user: UserRepository
+    private val task: TaskRepository, private val user: UserRepository
 ) : PatrolDetailUseCase {
     override suspend fun getDetailPatrol(id: Long): Flow<UiState<PatrolDetailModel>> {
         return task.getDetailPatrol(id).map {
             it.mapToUiState { data ->
                 PatrolDetailModel(
                     data.id,
-                    data.title,
+                    data.judul,
                     data.status,
                     data.sprin,
                     data.tanggal,
                     data.jam,
-                    data.tujuan,
-                    data.events.map { event ->
-                        PatrolEventModel(
-                            event.id,
-                            event.image,
-                            event.summary,
-                            event.title,
-                            event.action,
-                            event.lat,
-                            event.long,
-                            event.createdAt,
-                            author = event.author
-                        )
-                    }, lead = data.lead
+                    data.alamat,
+                    lead = data.ketua
                 )
+            }
+        }
+    }
+
+    override suspend fun getEventList(patrolId: Long): Flow<UiState<List<PatrolEventModel>>> {
+        return task.getEventList(patrolId).map {
+            it.mapToUiState { list ->
+                list.map { data ->
+                    PatrolEventModel(
+                        title = data.title,
+                        summary = data.summary,
+                        action = data.action,
+                        author = data.author,
+                        createdAt = data.createdAt,
+                        id = data.id
+                    )
+                }
             }
         }
     }
