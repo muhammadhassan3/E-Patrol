3e604d08152644dcb4a6bd386ef1dafeb47bb3a7
muhammhassan <muhammadhassan7788@gmail.com>
2023-09-13 12:39:20 +0700

    Change serialized response key

--- core/src/main/java/com/muhammhassan/epatrol/core/model/LoginResponse.kt
+++ core/src/main/java/com/muhammhassan/epatrol/core/model/LoginResponse.kt
@@ -1,6 +1,9 @@
 package com.muhammhassan.epatrol.core.model
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginResponse(
     val user: UserModel,
+    @SerializedName("access_token")
     val token: String
 )
